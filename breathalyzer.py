97bb0c537c1db72912765d8d2159173ce44ab9ff
Kevin <pi@raspberrypi.(none)>
2014-06-20 22:29:47 +0000

    updated code, add necessary modules

--- breathalyzer.py
+++ breathalyzer.py
@@ -1,19 +1,19 @@
-from RPiLcdBackpack import AdafruitLcd
+#from RPiLcdBackpack import AdafruitLcd
 from time import sleep
 from Adafruit_ADS1x15 import ADS1x15
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)					' numbering scheme
-GPIO.cleanup()							' reset channels
+GPIO.setmode(GPIO.BCM)					# numbering scheme
+GPIO.cleanup()							# reset channels
 GPIO.setwarnings(False)
-GPIO.setup(17,GPIO.OUT)					' set the output pin
+GPIO.setup(17,GPIO.OUT)					# set the output pin
 
-ADS115 = 0x01							' 16-bit ADC ADS115
-gain = 4096								' +/- 4.096V
-sps = 250								' samples per second
-adc = ADS1x15(ic=ADS115)				' initialize ADC with default I2C
+ADS115 = 0x01							# 16-bit ADC ADS115
+gain = 4096								# +/- 4.096V
+sps = 250								# samples per second
+#adc = ADS1x15(ic=ADS115)				# initialize ADC with default I2C
 
-bLCD = AdafruitLcd()					' LCD object
+#bLCD = AdafruitLcd()					# LCD object
 
 
 
@@ -22,21 +22,23 @@ while True:
 	
 	input = raw_input("Enter Input: ")
 	
-	if input.tolower() == "w":
-		'GPIO.output(17,GPIO.HIGH)
-	elif input.tolower() == "a":
-		'GPIO.output(17,GPIO.LOW)
-	elif input.tolower() == "l":
-		'lcd test
-		'lcd.blink(False)
-		'lcd.cursor(False)
-		'lcd.clear()
-		'lcd.backlight(True)
-		'lcd.message(string)
-		'sleep(x)
-	elif input.tolower() == "a":
-		'test read
-		'volts = adc.readADCSingleEnded(0,gain,sps) / 1000
-	elif input.tolower() == "q":
+	if input.lower() == "w":
+		print "high"
+		GPIO.output(17, True)
+	elif input.lower() == "s":
+		print "low"
+		GPIO.output(17, False)
+	#elif input.lower() == "l":
+		#lcd test
+		#lcd.blink(False)
+		#lcd.cursor(False)
+		#lcd.clear()
+		#lcd.backlight(True)
+		#lcd.message(string)
+		#sleep(x)
+	#elif input.tolower() == "a":
+		#test read
+		#volts = adc.readADCSingleEnded(0,gain,sps) / 1000
+	elif input.lower() == "q":
 		break
 	
