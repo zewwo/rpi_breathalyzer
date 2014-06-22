331d93b691930e1fed8e51fa93d664bd4860502b
Kevin Nguyen <azn_goku_91@hotmail.com>
2014-06-21 21:34:04 -0600

    added a test for LCD, removed button LEDS

--- breathalyzer.py
+++ breathalyzer.py
@@ -1,4 +1,4 @@
-#from RPiLcdBackpack import AdafruitLcd
+from RPiLcdBackpack import AdafruitLcd
 from time import sleep
 from Adafruit_ADS1x15 import ADS1x15
 import RPi.GPIO as GPIO
@@ -6,30 +6,25 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)							# numbering scheme
 GPIO.setwarnings(False)
 
-fLED = 17										# pin for SLOW FADING LED
-rLED = 22										# pin for RED LED
-rButton = 24									# pin for RED momentary switch 
-bButton = 25									# pin for BLUE momentary switch
-rButLED = 23									# pin for the RED momentary switch LED
-bButLED = 27									# pin for the BLUE momentary switch LED
+fLED = 17								# pin for SLOW FADING LED
+rLED = 22								# pin for RED LED
+rButton = 24								# pin for RED momentary switch 
+bButton = 25								# pin for BLUE momentary switch
 
 GPIO.setup(fLED, GPIO.OUT)						# setup the output pins
 GPIO.setup(rLED, GPIO.OUT)
-GPIO.setup(rButLED, GPIO.OUT)						
-GPIO.setup(bButLED, GPIO.OUT)
-GPIO.setup(rButton, GPIO.IN)					# setup input pins
+GPIO.setup(rButton, GPIO.IN)						# setup input pins
 GPIO.setup(bButton, GPIO.IN)
 
 
-ADS115 = 0x01									# 16-bit ADC ADS115
-gain = 4096										# +/- 4.096V
-sps = 250										# samples per second
+ADS115 = 0x01								# 16-bit ADC ADS115
+gain = 4096								# +/- 4.096V
+sps = 250								# samples per second
 #adc = ADS1x15(ic=ADS115)						# initialize ADC with default I2C
 
-#bLCD = AdafruitLcd()							# LCD object
+bLCD = AdafruitLcd()							# LCD object
 
 input = ""
-
 
 while input.lower() != "q":
 	print "\nW - LED Test #1\t\tS - LED Test #2\t\tL - LCD Test\nA - Alcohol Test\tB - Button Test\t\tQ - End Script\n"
@@ -44,19 +39,22 @@ while input.lower() != "q":
 		print "\nFADE OFF -- RED ON"
 		GPIO.output(fLED, False)
 		GPIO.output(rLED, True)
-	#elif input.lower() == "l":					# LCD test
-		#print "\nlcd test"
-		#lcd.blink(False)
-		#lcd.cursor(False)
-		#lcd.clear()
-		#lcd.backlight(True)
-		#lcd.message("testing!")
-		#sleep(2)
+	elif input.lower() == "l":					# LCD test
+		lcdInput = ""
+		bLCD.blink(False)
+		bLCD.cursor(False)
+		bLCD.backlight(True)
+		
+		while lcdInput.lower() != "exit":
+			lcdInput = raw_input("Send String to LCD (exit to go back) : ")
+			bLCD.clear()
+			bLCD.message(lcdInput)
+			sleep(0.5)
 	#elif input.lower() == "a":					# MQ3 alcohol sensor test
 		#print "\nsensor reading test"
 		#volts = adc.readADCSingleEnded(0,gain,sps) / 1000
 	elif input.lower() == "b":					# button test
-		print "\nBUTTON TESTING -- \nRED -> TEST PRINT -- BLUE -> EXIT TEST"
+		print "\nRED BUTTON -> TEST PRINT -- BLUE BUTTON -> EXIT TEST"
 		#GPIO.output(rButton, True)
 		GPIO.output(rButLED, True)				# turn on button LEDS
 		GPIO.output(bButLED, True)
@@ -64,15 +62,13 @@ while input.lower() != "q":
 		while GPIO.input(bButton) == False:
 			if GPIO.input(rButton):				# read button presses
 				print "Red is Pressed!"
-				sleep(0.5)						# debouncing
+				sleep(0.5)				# debouncing
 		print "Blue is Pressed!"
-		GPIO.output(rButLED, False)				# turn off button LEDS
-		GPIO.output(bButLED, False)
+
 		
 	sleep(0.1)
 
-GPIO.output(fLED, False)
+GPIO.output(fLED, False)						# turn off existing LEDs
 GPIO.output(rLED, False)
-#GPIO.output(rButton, False)	
 GPIO.cleanup()
 
